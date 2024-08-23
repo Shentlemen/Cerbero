7d795ac6b223858a06ad73e40918e2da5f973360
Shentlemen <germanrodao@gmail.com>
2024-08-23 16:21:29 -0300

    log in y estilizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,58 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxParticlesModule, NgParticlesService } from '@tsparticles/angular';
+import { loadSlim } from '@tsparticles/slim';
+import { MoveDirection, IOptions, RecursivePartial } from '@tsparticles/engine';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule para usar ngModel
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgxParticlesModule, FormsModule], // Asegúrate de agregar FormsModule aquí
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  username: string = '';
+  password: string = '';
 
-  navigateToMenu() {
-    this.router.navigate(['/menu']);
+  constructor(private router: Router, private readonly ngParticlesService: NgParticlesService) {}
+
+  particlesOptions: RecursivePartial<IOptions> = {
+    particles: {
+      number: {
+        value: 50,
+      },
+      size: {
+        value: 3,
+      },
+      move: {
+        enable: true,
+        direction: MoveDirection.none,
+        speed: 1,
+        random: true,
+        straight: false,
+      },
+      links: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+    },
+  };
+
+  ngOnInit(): void {
+    this.ngParticlesService.init(async (engine) => {
+      await loadSlim(engine);
+      console.log("tsParticles engine loaded successfully");
+    });
+  }
+
+  onSubmit(): void {
+    console.log("Usuario:", this.username);
+    console.log("Contraseña:", this.password);
+    this.router.navigate(['/menu']);  // Redirige al menú
   }
 }
