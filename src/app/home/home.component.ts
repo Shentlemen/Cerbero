2963d8c51208752ca63ceff7154e9876f111ae8a
Shado737 <31906098+SenpaiTronko@users.noreply.github.com>
2024-09-17 16:29:55 -0300

    cambios estilos home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,41 +20,24 @@ export class HomeComponent {
   constructor(private router: Router, private readonly ngParticlesService: NgParticlesService) {}
 
   particlesOptions: RecursivePartial<IOptions> = {
+    fpsLimit: 120,
     particles: {
       number: {
-        value: 50,
+        value: 80,
         density: {
           enable: true,
-        },
+        }
       },
       color: {
-        value: ["#4ca1af", "#2c3e50"],
+        value: ["#4ca1af", "#2c3e50", "#16a085", "#8e44ad", "#2980b9"],
       },
-      size: {
-        value: { min: 2, max: 5 },
-        animation: {
-          enable: true,
-          speed: 5,
-          startValue: "min",
-          sync: false,
-        },
-      },
-      move: {
-        enable: true,
-        direction: "none",
-        speed: 6,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        straight: false,
-      },
-      links: {
-        enable: true,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1,
+      shape: {
+        type: ["circle", "triangle", "polygon"],
+        options: {
+          polygon: {
+            sides: 6
+          }
+        }
       },
       opacity: {
         value: 0.5,
@@ -62,31 +45,89 @@ export class HomeComponent {
           enable: true,
           speed: 1,
           startValue: "min",
-          sync: false,
-        },
+          sync: false
+        }
       },
-      shape: {
-        type: "polygon",
-        options: {
-          sides: {
-            value: 4,
-          },
-        },
+      size: {
+        value: { min: 1, max: 5 },
+        animation: {
+          enable: true,
+          speed: 4,
+          startValue: "min",
+          sync: false
+        }
       },
+      links: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: false,
+        straight: false,
+        outModes: {
+          default: "out"
+        },
+        attract: {
+          enable: true,
+          rotate: { x: 600, y: 1200 }
+        }
+      }
     },
     interactivity: {
+      detectsOn: "window", // Detectar eventos en toda la ventana
       events: {
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
         onClick: {
           enable: true,
-          mode: "push",  // Modo "push" para crear más partículas
+          mode: "push"
         },
+        onDiv: [{ // Interacción con elementos específicos
+          selectors: ['#logo', '#loginBox'], // IDs de los elementos
+          enable: true,
+          mode: "repulse",
+          type: "rectangle"
+        }],
+        resize: { // Cambiado para que resize sea un objeto
+          enable: true, // Ahora se usa un objeto con la propiedad enable
+          delay: 0.5
+        }
       },
       modes: {
-        push: {
-          quantity: 4,  // Cantidad de partículas que se crearán al hacer clic
+        grab: {
+          distance: 400,
+          links: {
+            opacity: 1
+          }
         },
-      },
-    },    
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        },
+        push: {
+          quantity: 4
+        },
+        remove: {
+          quantity: 2
+        }
+      }
+    },
+    detectRetina: true
   };
 
   ngOnInit(): void {
@@ -100,7 +141,6 @@ export class HomeComponent {
   onSubmit(): void {
     console.log("Usuario:", this.username);
     console.log("Contraseña:", this.password);
-    // Cambia esta línea para redirigir directamente al dashboard
     this.router.navigate(['/menu/dashboard']);
   }
 }
