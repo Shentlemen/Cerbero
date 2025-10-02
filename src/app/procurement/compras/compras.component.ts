bd42ba1fa463f163c03b39b010e782bf94dfae96
Shentleman <116687466+Shentlemen@users.noreply.github.com>
2025-10-02 14:45:40 -0300

    correccion bug maskara de currency, punto de miles

--- src/app/procurement/compras/compras.component.ts
+++ src/app/procurement/compras/compras.component.ts
@@ -754,7 +754,8 @@ export class ComprasComponent implements OnInit {
     
     return new Intl.NumberFormat('es-ES', {
       minimumFractionDigits: 2,
-      maximumFractionDigits: 2
+      maximumFractionDigits: 2,
+      useGrouping: true
     }).format(monto);
   }
 
